f901c7869b278fb7b7a525f31f0dd8448b5115c3
Spencer <lava.cast1@gmail.com>
2021-12-11 20:13:11 -0700

    UpadateTest1

---
+++ Personal Site Resume.js
@@ -0,0 +1,8 @@
+function TextEducation() {
+  var text = document.getElementById("Education");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
